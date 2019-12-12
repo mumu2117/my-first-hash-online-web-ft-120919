f6b06adbb13275ee57bb6d2b40c8547442940725
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 14:12:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -42,6 +42,7 @@ shipping_manifest["gun powder"] = 4
 
   # add 4 gun powder to the shipping_manifest hash below
 
+ shipping_manifest # return the shipping_manifest hash below
 
 
   # return the shipping_manifest hash below
